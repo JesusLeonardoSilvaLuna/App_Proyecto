bf48a267e62c261408e7cda7808a5152c6c59d31
Leonardo609 <leoluna609@gmail.com>
2024-05-20 09:42:52 -0600

    modified:   client/src/components/list/List.jsx

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,49 +1,56 @@
 import {
-    Accordion,
-    AccordionBody,
-  } from "react-bootstrap";
-  import { useRef, useState } from "react";
-  import ListItem from "../listItem/ListItem";
-  import "./list.scss";
-  
-  export default function List({ list }) {
-    const [isMoved, setIsMoved] = useState(false);
-    const [slideNumber, setSlideNumber] = useState(0);
-    const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
-  
-    const listRef = useRef();
-  
-    const handleClick = (direction) => {
-      setIsMoved(true);
-      let distance = listRef.current.getBoundingClientRect().x - 50;
-      if (direction === "left" && slideNumber > 0) {
-        setSlideNumber(slideNumber - 1);
-        listRef.current.style.transform = `translateX(${230 + distance}px)`;
-      }
-      if (direction === "right" && slideNumber < 10 - clickLimit) {
-        setSlideNumber(slideNumber + 1);
-        listRef.current.style.transform = `translateX(${-230 + distance}px)`;
-      }
-    };
-    return (
-      <div className="list">
-        <span className="listTitle">{list.title}</span>
-        <div className="wrapper">
-          <Accordion
-            className="sliderArrow left"
-            onClick={() => handleClick("left")}
-            style={{ display: !isMoved && "none" }}
-          />
-          <div className="container" ref={listRef}>
-            {list.content.map((item, i) => (
-              <ListItem index={i} item={item} />
-            ))}
-          </div>
-          <AccordionBody
-            className="sliderArrow right"
-            onClick={() => handleClick("right")}
-          />
-        </div>
-      </div>
-    );
+  Accordion,
+  AccordionBody,
+} from "react-bootstrap";
+import { useRef, useState } from "react";
+import ListItem from "../listItem/ListItem";
+import "./list.scss";
+
+export default function List({ list }) {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
+  // Verificación de que list.content es un array
+  if (!Array.isArray(list.content)) {
+    console.error("Expected list.content to be an array, but got:", list.content);
+    return null; // O maneja el error como creas conveniente
   }
+
+  return (
+    <div className="list">
+      <span className="listTitle">{list.title}</span>
+      <div className="wrapper">
+        <Accordion
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          {list.content.map((item, i) => (
+            <ListItem key={i} index={i} item={item} />
+          ))}
+        </div>
+        <AccordionBody
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+}
