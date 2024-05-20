276b8f93492a2dd07ecaa9fe5efb72ac75b98896
Leonardo609 <leoluna609@gmail.com>
2024-05-20 11:38:09 -0600

    modified:   client/src/components/list/List.jsx

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,7 +1,7 @@
 import {
-  Accordion,
-  AccordionBody,
-} from "react-bootstrap";
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
 import { useRef, useState } from "react";
 import ListItem from "../listItem/ListItem";
 import "./list.scss";
@@ -9,7 +9,7 @@ import "./list.scss";
 export default function List({ list }) {
   const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
-  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
+  const [clickLimit] = useState(window.innerWidth / 230);
 
   const listRef = useRef();
 
@@ -18,35 +18,33 @@ export default function List({ list }) {
     let distance = listRef.current.getBoundingClientRect().x - 50;
     if (direction === "left" && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
-      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      listRef.current.style.transform = translateX($230 + distancepx);
     }
     if (direction === "right" && slideNumber < 10 - clickLimit) {
       setSlideNumber(slideNumber + 1);
-      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      listRef.current.style.transform = translateX($-230 + distancepx);
     }
   };
-
-  // Verificación de que list.content es un array
-  if (!Array.isArray(list.content)) {
-    console.error("Expected list.content to be an array, but got:", list.content);
-    return null; // O maneja el error como creas conveniente
-  }
 
   return (
     <div className="list">
       <span className="listTitle">{list.title}</span>
       <div className="wrapper">
-        <Accordion
+        <ArrowBackIosOutlined
           className="sliderArrow left"
           onClick={() => handleClick("left")}
           style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          {list.content.map((item, i) => (
-            <ListItem key={i} index={i} item={item} />
-          ))}
+          {Array.isArray(list.content) ? (
+            list.content.map((item, i) => (
+              <ListItem key={i} item={item} />
+            ))
+          ) : (
+            <div>No items available</div> // Opcional: manejar el caso donde no hay elementos
+          )}
         </div>
-        <AccordionBody
+        <ArrowForwardIosOutlined
           className="sliderArrow right"
           onClick={() => handleClick("right")}
         />
