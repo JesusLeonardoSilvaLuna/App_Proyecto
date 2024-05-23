1d4a29eda5d2941042ab30779ce8fbd404c8c6e5
Leonardo609 <leoluna609@gmail.com>
2024-05-23 12:15:10 -0600

    modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,32 @@
-import "./app.scss";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import InscripcionForm from "./pages/inscripciones/Inscripciones";
+import Eventos from './pages/eventos/Eventos'; 
+import Ciclistas from './pages/ciclistas/Ciclistas'; 
 import { useContext } from "react";
 import { AuthContext } from "./authContext/AuthContext";
-import InscripcionForm from "./pages/inscripciones/Inscripciones";
-import Eventos from "./pages/eventos/Eventos";
-import Ciclistas from "./pages/ciclistas/Ciclistas";
+import "./app.scss";
 
 const App = () => {
   const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Redirect to="/login" />}
-        </Route>
-        <Route path="/register">
-          {!user ? <Register /> : <Redirect to="/" />}
-        </Route>
-        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        <Route path="/" element={user ? <Home /> : <Redirect to="/login" />} />
+        <Route path="/register" element={!user ? <Register /> : <Redirect to="/" />} />
+        <Route path="/login" element={!user ? <Login /> : <Redirect to="/" />} />
         <Route path="/eventos" element={<Eventos />} />
         <Route path="/inscripciones" element={<InscripcionForm />} />
         <Route path="/ciclistas" element={<Ciclistas />} />
-
         {user && (
           <>
-            <Route path="/movies">
-              <Home type="movie" />
-            </Route>
-            <Route path="/series">
-              <Home type="series" />
-            </Route>
-            <Route path="/watch">
-              <Watch />
-            </Route>
+            <Route path="/movies" element={<Home type="movie" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
           </>
         )}
       </Switch>
