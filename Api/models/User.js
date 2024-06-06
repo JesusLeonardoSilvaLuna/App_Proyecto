c61f7ad39633c3d4124d404d02550d765e3b3b56
Leonardo609 <leoluna609@gmail.com>
2024-06-06 13:26:01 -0600

    modified:   Api/models/User.js

--- Api/models/User.js
+++ Api/models/User.js
@@ -6,7 +6,10 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
     profilePic: { type: String, default: "" },
+    coverPic: { type: String, default: "" },
     isAdmin: { type: Boolean, default: false },
+    role: { type: String, enum: ["Ciclista", "Aficionado", "Organizador", "Juez", "Administrador"], required: true},
+    ciclistaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ciclista' }
   },
   { timestamps: true }
 );
