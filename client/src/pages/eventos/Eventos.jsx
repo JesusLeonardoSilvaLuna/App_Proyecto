6b54405ed4f5df5006f592680b6562963d23aafc
Leonardo609 <leoluna609@gmail.com>
2024-06-09 20:56:03 -0600

    modified:   client/src/pages/eventos/Eventos.jsx

--- client/src/pages/eventos/Eventos.jsx
+++ client/src/pages/eventos/Eventos.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '../../components/navbar/Navbar';
 import './eventos.scss';
@@ -21,14 +21,13 @@ const Eventos = () => {
   const [eventos, setEventos] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedEvento, setSelectedEvento] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const intervalRef = useRef(null);
 
   useEffect(() => {
     const fetchEventos = async () => {
       try {
-        const response = await axios.get('https://app-proyecto-api.vercel.app/eventos/obtener');
+        const response = await axios.get('https://app-proyecto-api.vercel.app/api/eventos/obtener');
         setEventos(response.data);
+        console.log(response.data); // Verificar que la URL de la imagen se recibe correctamente
       } catch (error) {
         console.error('Error fetching eventos:', error);
       }
@@ -37,26 +36,9 @@ const Eventos = () => {
     fetchEventos();
   }, []);
 
-  useEffect(() => {
-    if (eventos.length > 0) {
-      startAutoSlide();
-    }
-    return stopAutoSlide;
-  }, [eventos]);
-
-  const startAutoSlide = () => {
-    intervalRef.current = setInterval(() => {
-      goToNext();
-    }, 5000);
-  };
-
-  const stopAutoSlide = () => {
-    clearInterval(intervalRef.current);
-  };
-
   const handleCardClick = async (id) => {
     try {
-      const response = await axios.get(`https://app-proyecto-api.vercel.app/eventos/obtener/${id}`);
+      const response = await axios.get(`https://app-proyecto-api.vercel.app/api/eventos/obtener/${id}`);
       setSelectedEvento(response.data);
       setShowModal(true);
     } catch (error) {
@@ -69,58 +51,43 @@ const Eventos = () => {
     setSelectedEvento(null);
   };
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? eventos.length - 1 : prevIndex - 1));
-  };
+  // Estilo para el área destacada 
+  const featuredStyle = { 
+    backgroundImage: 'url("https://markrossstudio.com/wp/wp-content/uploads/2014/09/MRoss_CPowersRacingB-e1410990826225.jpg")', 
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center', 
+    borderRadius: '10px', 
+    height: '300px', // Ajusta la altura según sea necesario 
+    marginBottom: '20px', // Espacio entre el área destacada y las tarjetas 
+  }; 
 
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % eventos.length);
-  };
-
-  // Estilo para el área destacada
-  const featuredStyle = {
-    backgroundImage: 'url("https://markrossstudio.com/wp/wp-content/uploads/2014/09/MRoss_CPowersRacingB-e1410990826225.jpg")',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    borderRadius: '10px',
-    height: '420px', // Ajusta la altura según sea necesario
-    marginBottom: '10px', // Espacio entre el área destacada y las tarjetas
-  };
-return (
+  return (
     <div>
       <div className="organizador">
         <Navbar />
       </div>
       <div className="featured" style={featuredStyle}></div>
       <div className="cards-container">
-        <button className="arrow arrow-left" onClick={goToPrevious}>&#8249;</button>
-        <div className="slider">
-          <div className="cards" style={{ transform: `translateX(-${currentIndex * (100 / eventos.length)}%), width: ${100 * eventos.length}%`}}>
-            {eventos.map((evento, index) => (
-              <Card
-                key={evento._id}
-                image={evento.imagen || 'https://via.placeholder.com/150'}
-                title={evento.nombre}
-                description={evento.descripcion}
-                onClick={() => handleCardClick(evento._id)}
-              />
-            ))}
-          </div>
-        </div>
-        <button className="arrow arrow-right" onClick={goToNext}>&#8250;</button>
+        {eventos.map(evento => (
+          <Card 
+            key={evento._id}
+            image={evento.imagen || 'https://via.placeholder.com/150'}
+            title={evento.nombre}
+            description={evento.descripcion}
+            onClick={() => handleCardClick(evento._id)}
+          />
+        ))}
       </div>
       {showModal && selectedEvento && (
         <div className="modal">
           <div className="modal-content">
             <span className="close" onClick={closeModal}>&times;</span>
-            <div>
-              <h2>{selectedEvento.nombre}</h2>
-            </div>
-            <div className="imagen">
-              <img src={selectedEvento.imagen || 'https://via.placeholder.com/150'} alt={selectedEvento.nombre} />
-            </div>
             <div className="evento-detalle">
+              <div className="imagen">
+                <img src={selectedEvento.imagen || 'https://via.placeholder.com/150'} alt={selectedEvento.nombre} />
+              </div>
               <div className="detalles">
+                <h2>{selectedEvento.nombre}</h2>
                 <p>{selectedEvento.descripcion}</p>
                 <p><strong>Fecha:</strong> {new Date(selectedEvento.fecha).toLocaleDateString()}</p>
                 <p><strong>Lugar:</strong> {selectedEvento.lugar}</p>
