036b2c5667ad0da32991cffa717448f210acab35
Leonardo609 <leoluna609@gmail.com>
2024-06-09 20:53:23 -0600

    modified:   client/src/pages/eventos/Eventos.jsx

--- client/src/pages/eventos/Eventos.jsx
+++ client/src/pages/eventos/Eventos.jsx
@@ -27,7 +27,7 @@ const Eventos = () => {
   useEffect(() => {
     const fetchEventos = async () => {
       try {
-        const response = await axios.get('https://app-proyecto-api.vercel.app/evento/obtener');
+        const response = await axios.get('https://app-proyecto-api.vercel.app/eventos/obtener');
         setEventos(response.data);
       } catch (error) {
         console.error('Error fetching eventos:', error);
@@ -56,7 +56,7 @@ const Eventos = () => {
 
   const handleCardClick = async (id) => {
     try {
-      const response = await axios.get(`https://app-proyecto-api.vercel.app/evento/obtener/${id}`);
+      const response = await axios.get(`https://app-proyecto-api.vercel.app/eventos/obtener/${id}`);
       setSelectedEvento(response.data);
       setShowModal(true);
     } catch (error) {
