7695bf2b11f745d6620a7ef9214be68d3faa4a71
Leonardo609 <leoluna609@gmail.com>
2024-06-11 09:31:48 -0600

    modified:   client/src/components/MenuOrganizador/Menu.jsx

--- client/src/components/MenuOrganizador/Menu.jsx
+++ client/src/components/MenuOrganizador/Menu.jsx
@@ -51,7 +51,7 @@ const Menu = () => {
         </div>
         <div className="right">
           <SearchIcon className="icon" />
-          <span>Mauricio</span>
+          <span>Angie</span>
           <NotificationsIcon className="icon" />
           <img
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
